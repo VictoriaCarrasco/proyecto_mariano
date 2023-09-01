bd20aeff262aaa2bf0ee8726db2d4be1d7635529
VictoriaCarrasco <victoriacarrascoc97@gmail.com>
2023-08-31 20:53:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myfirstapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
